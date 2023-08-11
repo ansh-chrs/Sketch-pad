afc6c732c8c799108f352a7648f59c25586645f5
Anshuman <chaurasiaji200@gmail.com>
2023-08-12 00:17:20 +0530

    Add feature to draw on grid using mouse pointer
    Add event listener to draw only when left mouse button is clicked and it
    hovers over the pixel blocks in that clicked state
    
    The drag event is disabled because it was causing problems if the pointer
    was pressed for too long
    
    Add functionality to chooose color of drawing

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 const canvas = document.querySelector('.innerContainer');
+const colorInput = document.querySelector('.color');
+let penColor = colorInput.value;
+colorInput.addEventListener('input', ()=> {
+  penColor = colorInput.value;
+});
 
-let count = 10;
 
+let count =10;
+let clickState;
 
+for (let i = 1; i <= count; i++) {
+  let row = document.createElement('div');
+  row.style.display = `flex`;
 
-for(let i = 1; i <= count ; i++ )
-{
-    let row =document.createElement('div');
-    row.style.display =  `flex`;
-    for(let j = 1; j <= count ; j++ )
- {
-   let grid = document.createElement('div');
-   grid.style.border = "1px solid white";
-   grid.style.flex = `1`;
+        for (let j = 1; j <= count; j++) {
+          let grid = document.createElement('div');
+          grid.style.border = "1px solid white";
+          grid.style.flex = `1`;
+          addEventListener("dragstart", (event) => {
+            event.preventDefault();   //Prevents pointer from dragging element on longer press
+          });
 
-   grid.addEventListener('mouseover',changer)
-   row.appendChild(grid);
- }
- canvas.appendChild(row);
+          grid.addEventListener('mousedown', (e) => {
+            clickState = 1;
+            painter(e);
+          });
+
+          grid.addEventListener('mouseup', () => { clickState = 0; });
+          grid.addEventListener('mouseover', painter);
+          row.appendChild(grid);
+        }
+  canvas.appendChild(row);
   row.style.flex = '1';
+
 }
 
-function changer(e)
-{
-  e.target.classList.add('hoverEvent');
-}
 
+
+function painter(e) {
+
+  if (clickState) { e.target.style.backgroundColor =`${penColor}` };
+
+}
