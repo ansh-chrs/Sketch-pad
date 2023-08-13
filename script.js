8db910ad70b437a40f6971050b6dde54cf94321d
Anshuman <chaurasiaji200@gmail.com>
2023-08-13 17:39:23 +0530

    Feature of changing grid size using slider & bug fixes
    Wrap the loops generating the grid, inside a function gridGenerator.
    gridGenerator takes value of number of pixels from slider. It is called
    when either the page loads or when there is change in slider value
    
    Whenever gridgenerator is called, the entire previous grid is removed
    using innerHTML function and the value of penState variable and
    clickState is reset and so that any previous pen settings do not
    interfere with the newer grid.
    
    Listen mouse up and mouse down event over entire document. Listening
    these events just on the grid was causing unwanted results whenever
    cursor was moved out and left mouse button was released.

--- script.js
+++ script.js
@@ -1,44 +1,90 @@
 const canvas = document.querySelector('.innerContainer');
 const colorInput = document.querySelector('.color');
+const resetbtn = document.querySelector('.reset');
+const mixBtn = document.querySelector('.mixNmatch');
+const eraseBtn = document.querySelector('.eraser');
+const gridSize = document.querySelector('.gridSize');
+let clickState;
+let penState = '';
+
 let penColor = colorInput.value;
-colorInput.addEventListener('input', ()=> {
+colorInput.addEventListener('input', () => {
   penColor = colorInput.value;
+  penState='';
 });
 
+gridSize.addEventListener('input', gridGenerator);
+window.addEventListener('load',gridGenerator);
+document.addEventListener('mousedown', () => { clickState = 1 })
+document.addEventListener('mouseup', () => { clickState = 0 })
 
-let count =10;
-let clickState;
+function gridGenerator() {
+  if (canvas.hasChildNodes) {
+    canvas.innerHTML = '';
+  }
+  penState = '';
+  clickState = 0;
+  let count = gridSize.value;
+      for (let i = 1; i <= count; i++) {
+        let row = document.createElement('div');
+        row.style.display = `flex`;
 
-for (let i = 1; i <= count; i++) {
-  let row = document.createElement('div');
-  row.style.display = `flex`;
+              for (let j = 1; j <= count; j++) {
+                let grid = document.createElement('div');
+                grid.classList.add('pixels');
+                grid.style.flex = "1";
+                grid.style.border = `1px solid white`;
+                resetbtn.addEventListener('click', () => { grid.style.backgroundColor = `black`, penState = ''; });
 
-        for (let j = 1; j <= count; j++) {
-          let grid = document.createElement('div');
-          grid.style.border = "1px solid white";
-          grid.style.flex = `1`;
-          addEventListener("dragstart", (event) => {
-            event.preventDefault();   //Prevents pointer from dragging element on longer press
-          });
+                grid.addEventListener("dragstart", (event) => {
+                  event.preventDefault();   //Prevents pointer from dragging element on longer press
+                });
 
-          grid.addEventListener('mousedown', (e) => {
-            clickState = 1;
-            painter(e);
-          });
+                grid.addEventListener('mousedown', (e) => {
+                  clickState = 1;
+                  painter(e);
+                });
+                grid.addEventListener('mouseover', painter);
+                row.appendChild(grid);
+              }
+              
+        canvas.appendChild(row);
+        row.style.flex = '1';
 
-          grid.addEventListener('mouseup', () => { clickState = 0; });
-          grid.addEventListener('mouseover', painter);
-          row.appendChild(grid);
-        }
-  canvas.appendChild(row);
-  row.style.flex = '1';
-
+      }
 }
 
 
+
+mixBtn.addEventListener('click', () => { penState = mixBtn.value; });
+eraseBtn.addEventListener('click', () => { penState = eraseBtn.value; });
 
 function painter(e) {
 
-  if (clickState) { e.target.style.backgroundColor =`${penColor}` };
+  if (clickState) {
 
+    if (penState === 'rainbow') {
+      e.target.style.backgroundColor = rainbowMaker();
+
+    }
+
+    else if (penState === 'eraser') {
+
+      e.target.style.backgroundColor = `black`;
+
+    }
+
+    else {
+      e.target.style.backgroundColor = `${penColor}`;
+    }
+  }
 }
+
+
+function rainbowMaker() {
+  return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  return colorCode;
+}
+
+
+
